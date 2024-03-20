d866bd97eadbffadc2432ecec942872f43bc52a3
kenjitheman <im@kenjitheman.dev>
2024-03-20 23:05:58 +0200

    feat: add speed insights && analytics
    fix: analytics error
    
    fix: analytics error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
     integrations: [
@@ -12,4 +11,8 @@ export default defineConfig({
         }),
         react(),
     ],
+    output: "server",
+    adapter: vercel({
+        webAnalytics: { enabled: true }
+    }),
 });
---
+++ src/components/More.tsx
@@ -0,0 +1,14 @@
+export default () => {
+    return (
+        <div className="flex flex-col items-start justify-between p-4 md:p-5 xl:p-6 w-full h-full">
+            <div className="flex flex-row items-center gap-2 p-2 md:p-0">
+                <div className="flex flex-col items-start gap-1">
+                    <h1 className="font-medium text-lg lg:text-xl text-primary-11">🌸 More</h1>
+                </div>
+            </div>
+            <div className="p-2 md:p-0">
+                If you're seeking a welcoming space where you can engage in meaningful conversations, make lasting friendships, and explore the diverse world of anime in a close-knit community, look no further than Hitori Anime Club. Join us, and together, let's make memories that will last a lifetime.
+            </div>
+        </div >
+    );
+};
